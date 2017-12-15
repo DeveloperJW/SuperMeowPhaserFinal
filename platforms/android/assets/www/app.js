0d9ae7218199a76aaf9424752e21a90eedff6b51
Fzuo <fzuo@luc.edu>
2017-12-15 11:58:40 -0600

    sign out

---
+++ platforms/android/assets/www/app.js
@@ -0,0 +1,13 @@
+(function(){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDiZjDbVuGQSRW1uzCmTAbqfH-CfLGuitI",
+        authDomain: "supermeow-dc20c.firebaseapp.com",
+        databaseURL: "https://supermeow-dc20c.firebaseio.com",
+        projectId: "supermeow-dc20c",
+        storageBucket: "supermeow-dc20c.appspot.com",
+        messagingSenderId: "689979976743"
+    };
+    firebase.initializeApp(config);
+
+}());
